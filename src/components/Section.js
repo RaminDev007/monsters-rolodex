f2d5ef68a4cf9234d5255fd9a0ce162cb99f7bdd
RaminDev007 <ramin.majidzada@yahoo.com>
2023-10-01 22:52:16 -0400

    first commit

---
+++ src/components/Section.js
@@ -0,0 +1,15 @@
+import React from "react";
+import styled from "styled-components";
+
+function Section() {
+  return <wrap>Section</wrap>;
+}
+
+export default Section;
+
+const wrap = styled.div`
+  width: 100vw;
+  height: 100vh;
+  background: orange;
+  //background-image: url()
+`;
